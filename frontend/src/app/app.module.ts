177a5ae7c0ad6c9cd686f15bb2768ae1c8329879
creativebull <creativebull0103@gmail.com>
2022-06-23 17:00:56 +0100

    Changing the design to the basic bootstrap 5 dashboard

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { SidebarComponent } from './main-components/sidebar/sidebar.component';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
