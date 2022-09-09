5612cdf84d94de103fac853bca8765fa0bc1af46
creativebull <creativebull0103@gmail.com>
2022-09-09 19:01:09 +0100

    Some production settings fixed and moving some HTML around

--- backend/database/migrations/2022_06_26_095806_create_tasks_table.php
+++ backend/database/migrations/2022_06_26_095806_create_tasks_table.php
@@ -18,7 +18,7 @@ return new class extends Migration {
             $table->uuid();
             $table->unsignedBigInteger('board_list_id');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('assigned_to');
+            $table->unsignedBigInteger('assigned_to')->nullable();
             $table->string('name');
             $table->text('description')->nullable();
             $table->integer('position')->default(0);
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appName: 'Get It Done',
-  apiUrl: 'http://api-get-it-done.ryantesting.co.uk/',
+  apiUrl: 'https://api-get-it-done.ryantesting.co.uk/',
   apiDebug: false,
 };
